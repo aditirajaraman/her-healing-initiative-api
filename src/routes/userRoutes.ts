d0f81f91e187ece0bb5aba7708b239c32906b71f
aditirajaraman <aditirajaraman10272008@gmail.com>
2025-02-15 23:22:36 -0500

    Get Individual Event Organizers

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -10,7 +10,7 @@ const userRoutes = Router();
 userRoutes.get("/users", (req: Request, res: Response) => {
   try {
     User.find().then(function(result){
-      console.log('Found Users...');
+      //console.log('Found Users...');
       res.send(result);
     })
   } catch (error: any) {
@@ -19,11 +19,40 @@ userRoutes.get("/users", (req: Request, res: Response) => {
   }
 });
 
+// Get event Orgaizers
+userRoutes.get("/users/GetUserEventOrganizer", (req: Request, res: Response) => {
+  try {
+    //User.find({}, { name:'$lastname', value:'$username', _id:0}).then(function(result){
+    //User.find().select('username firstname lastname').then(function(result){
+    User.aggregate([
+      {
+        $project: {
+          _id: 0, // Exclude the _id field
+          name: {
+            $concat: [
+              "$lastname",
+              ", ",
+              "$firstname"
+            ]
+          },
+          value : '$username'
+        }
+      }
+    ]).then(function(result){
+      //console.log('Found Users...');
+      res.send(result);
+    })
+  } catch (error: any) {
+    console.error('Error Finding Event Orgainizers:' + error.message);
+    res.send(error.message);
+  }
+});
+
 // Get a User by id
 userRoutes.get("/users/:id", (req: Request, res: Response) => {
   try {
     User.findById(req.params.id).then(function(result){
-      console.log('Found User...');
+      //console.log('Found User...');
       res.send(result);
     })
   } catch (error: any) {
@@ -36,12 +65,12 @@ userRoutes.get("/users/:id", (req: Request, res: Response) => {
 userRoutes.post("/users", (req: Request, res: Response) => {
    //var user = new User(req.body);
    //const formattedDate: Date = format(req.body.birthdate, 'dd/MM/yyyy');
-   console.log("Initiated Post Request...");
-   console.log(req.body);
+   //console.log("Initiated Post Request...");
+   //console.log(req.body);
    //console.log(format(req.body.birthdate, "yyyy-MM-dd"));
    //const formattedDate = parse(req.body.birthdate, 'dd/MM/yyyy', new Date());
    const formattedDate = parse(format(req.body.birthdate, "yyyy-MM-dd"), 'yyyy-MM-dd', new Date());
-   console.log(formattedDate);
+   //console.log(formattedDate);
    let user = new User({
     firstname: req.body.firstname,
     lastname: req.body.lastname,
@@ -56,7 +85,7 @@ userRoutes.post("/users", (req: Request, res: Response) => {
    /*----------------------Check for Registered Users ----------------*/
    //User.exists( {username: 'aditir'});
    User.exists({username: user.username}).then(result => { 
-      console.log(result) 
+      //console.log(result) 
       foundUser = ( result == null )  ? false:true;
       if (foundUser)
       {
@@ -75,14 +104,14 @@ userRoutes.post("/users", (req: Request, res: Response) => {
       }
    })
 
-   console.log('foundUser: '+ foundUser + ' username:' +  user.username);
+   //console.log('foundUser: '+ foundUser + ' username:' +  user.username);
    
    
 });
 
 //Update a book by ID
 userRoutes.put("/users/:id", (req: Request, res: Response) => {
-  console.log(req.body);
+  //console.log(req.body);
   let user = User.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -99,9 +128,9 @@ userRoutes.put("/users/:id", (req: Request, res: Response) => {
 // Delete a book by ID
 userRoutes.delete("/users/:id", (req: Request, res: Response) => {
   try {
-    console.log(req.params.UserName);
+    //console.log(req.params.UserName);
     User.deleteOne( {username: req.params.UserName}).then(function(result){
-      console.log('Deleted User...');
+      //console.log('Deleted User...');
       res.send("Successfully Deleted User");
     })
   } catch (error: any) {
